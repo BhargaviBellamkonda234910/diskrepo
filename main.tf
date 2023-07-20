45e1bb50e253fe1ca361cbed574d7c6d9935136e
BhargaviBellamkonda234910 <110422372+BhargaviBellamkonda234910@users.noreply.github.com>
2023-07-20 19:55:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "azurerm_resource_group" "example" {
-  name     = "diskrg234"
-  location = "West Europe"
-  tags = {owner = "devopsteam1"}
+  name     = var.rgname
+  location = var.loc
+
 }
 
 resource "azurerm_managed_disk" "example" {
-  name                 = "datadisk234"
+  name                 = var.diskname
   location             = azurerm_resource_group.example.location
   resource_group_name  = azurerm_resource_group.example.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "40"
+  storage_account_type = var.accounttype
+  create_option        = var.create
+  disk_size_gb         = var.size
 }
