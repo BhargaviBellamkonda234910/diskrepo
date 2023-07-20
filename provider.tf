6772c5359b9a2b54f839499a0805cf720a63c3ec
BhargaviBellamkonda234910 <110422372+BhargaviBellamkonda234910@users.noreply.github.com>
2023-07-20 14:12:49 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  
-  
-  client_id       = "ba03377c-84fc-43cf-8704-a440d3202f5a"
-  client_secret   = "dGf8Q~if7mlwh1~8royQsyLOC7SukIrqF1hlgdva"
-  tenant_id       = "ca026c71-dbb3-4b9b-ac1b-ea6dd03d732d"
-  subscription_id = "586d08c9-d093-41d5-bfe8-bae11cc1c37f"
 }
